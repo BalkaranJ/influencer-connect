75d8cb255ea91dbb858d24f25abd7099dc5cb8a0
Balkaran <balkaranjaswal@gmail.com>
2020-09-30 21:05:45 +0000

    initial react set up

---
+++ client/influencer-react/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import { Hello } from './components/hello'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        < Hello />
+      </div>
+    );
+  }
+}
+
+export default App;
