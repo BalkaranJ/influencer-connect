8e81891ec66ba044c983b606f212c62e7a1a6529
whyttte <fogboruche@yahoo.com>
2020-09-30 19:45:20 +0000

    WIP

---
+++ db-test.js
@@ -0,0 +1,21 @@
+const controller = require("./lib/controllers/campaignsController");
+const db = require("./lib/db");
+const userObj = {taskId: 1, userId: 2};
+let resObj = {};
+controller
+  .setUpdateStatus(db, userObj)
+  .then((data) => {
+    console.log("//////////////////////////////", data);
+    if (data.rowCount > 0) {
+      resObj.success = 200;
+      resObj.data = data.rows;
+      data.json(resObj);
+    } else {
+      throw new Error("No rows deleted");
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+    resObj.status = 500;
+    resObj.error = err.message;
+  });
